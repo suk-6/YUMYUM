32d78ad4d2838219156dca840e47ebbc132ae845
typidoyun <typidoyun@gmail.com>
2024-03-12 20:22:29 +0900

    add: neis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NeisService } from './neis/neis.service';
+import { NeisController } from './neis/neis.controller';
+import { NeisModule } from './neis/neis.module';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [NeisModule],
+    controllers: [AppController, NeisController],
+    providers: [AppService, NeisService],
 })
 export class AppModule {}
---
+++ src/neis/neis.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('neis')
+export class NeisController {}
