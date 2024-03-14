bc480f8a3aa64c3867d3d9f8974420dbdeec3f30
suk-6 <me@suk.kr>
2024-03-14 19:07:37 +0900

    Feat: getDietInfo method and add getTodayDiet method

--- src/neis/neis.service.ts
+++ src/neis/neis.service.ts
@@ -6,11 +6,33 @@ import { lastValueFrom } from 'rxjs';
 export class NeisService {
     constructor(private readonly httpService: HttpService) {}
 
-    public async getDietInfo() {
-        const url = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.NEIS_API_KEY}&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531328&MLSV_YMD=20240312`;
+    public async getDietInfo(today: string) {
+        const url = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.NEIS_API_KEY}&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531328&MLSV_YMD=${today}`;
         const pipe = this.httpService.get(url).pipe();
         const response = await lastValueFrom(pipe);
 
         return response.data;
     }
+
+    public async getTodayDiet() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = now.getMonth() + 1;
+        const day = now.getDate();
+
+        const today = `${year}${month < 10 ? `0${month}` : month}${day < 10 ? `0${day}` : day}`;
+
+        const dietInfo = (await this.getDietInfo(today)).mealServiceDietInfo[1]
+            .row[0];
+
+        const diet = dietInfo.DDISH_NM.split('<br/>').map((item: string) =>
+            item.trim(),
+        );
+        const kcal = dietInfo.CAL_INFO;
+    }
 }
+
+(() => {
+    const neis = new NeisService(new HttpService());
+    neis.getTodayDiet();
+})();
